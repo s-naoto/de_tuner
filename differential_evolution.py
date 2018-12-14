c0e8f03125e878ba5236011040cf6a81dc16bd32
s-naoto <naoto.suzuki@sensy.ai>
2018-12-14 13:56:56 +0900

    first commit

---
+++ differential_evolution.py
@@ -0,0 +1,270 @@
+import numpy as np
+import multiprocessing as mp
+from logging import getLogger
+import datetime
+from functools import partial
+
+logger = getLogger('__name__')
+
+
+class DE(object):
+    """
+    Differential Evolution
+    """
+
+    def __init__(self, objective_function, pop, ndim, lower_limit, upper_limit, minimize=True):
+        """
+
+        :param objective_function: f(x) callable function
+        :param pop: number of populations
+        :param ndim: dimension of x
+        :param lower_limit: lower limit of search space 1d-array
+        :param upper_limit: upper limit of search space 1d-array
+        :param minimize: minimize flag. if the problem is minimization, then set True.
+                                        otherwise set False and turning as maximization.
+        """
+        self._of = objective_function
+        self._p = pop
+        self._nd = ndim
+        self._x_current = None
+        self._low_lim = lower_limit
+        self._up_lim = upper_limit
+        self._f_current = None
+        self._is_minimize = minimize
+
+    def initialization(self, x_init=None):
+        """
+
+        :param x_init: initial value of x (optional)
+        :return:
+        """
+        # initialize x
+        if x_init:
+            self._x_current = x_init
+        else:
+            self._x_current = np.random.rand(self._p, self._nd) * (self._up_lim - self._low_lim) + self._low_lim
+
+    def _update(self, u, fu):
+        """
+
+        :param u: trial vectors
+        :param fu: evaluation values of trial vectors
+        :return:
+        """
+        # score is better than current
+        q1 = fu < self._f_current if self._is_minimize else fu > self._f_current
+        # over lower limit
+        q2 = np.any(u < self._low_lim, axis=1)
+        # over upper limit
+        q3 = np.any(u > self._up_lim, axis=1)
+        # q1 ^ ~q2 ^ ~q3
+        q = np.where(q1 * ~q2 * ~q3)
+
+        # update current values
+        self._f_current[q] = fu[q].copy()
+        self._x_current[q] = u[q].copy()
+
+    def _get_mutant_vector(self, current, method, num, sf):
+        """
+
+        :param current: current index of population
+        :param method: mutation method
+        :param num: number of mutant vectors
+        :param sf: scaling factor
+        :return:
+        """
+        # mutant vector
+        # best
+        if method == 'best':
+            r_best = np.argmin(self._f_current) if self._is_minimize else np.argmax(self._f_current)
+            r = [r_best]
+            r += np.random.choice([n for n in range(self._p) if n != r_best], 2 * num, replace=False).tolist()
+            v = self._x_current[r[0]] \
+                + sf * np.sum([self._x_current[r[m + 1]] - self._x_current[r[m + 2]] for m in range(num)], axis=0)
+
+        # rand
+        elif method == 'rand':
+            r = np.random.choice(range(self._p), 2 * num + 1, replace=False).tolist()
+            v = self._x_current[r[0]] \
+                + sf * np.sum([self._x_current[r[m + 1]] - self._x_current[r[m + 2]] for m in range(num)], axis=0)
+
+        # current-to-rand
+        elif method == 'current-to-rand':
+            r = [current]
+            r += np.random.choice([n for n in range(self._p) if n != current], 2 * num + 1, replace=False).tolist()
+            v = self._x_current[r[0]] \
+                + np.random.rand() * (self._x_current[r[1]] - self._x_current[r[0]]) \
+                + sf * np.sum([self._x_current[r[m + 2]] - self._x_current[r[m + 3]] for m in range(num)], axis=0)
+
+        # current-to-best
+        elif method == 'current-to-best':
+            r_best = np.argmin(self._f_current) if self._is_minimize else np.argmax(self._f_current)
+            r = [r_best, current]
+            r += np.random.choice([
+                n for n in range(self._p) if n not in [r_best, current]], 2 * num, replace=False).tolist()
+            v = self._x_current[r[0]] \
+                + sf * (self._x_current[r[1]] - self._x_current[r[0]]) \
+                + sf * np.sum([self._x_current[r[m + 2]] - self._x_current[r[m + 3]] for m in range(num)], axis=0)
+
+        else:
+            raise ValueError('invalid `method`: {}'.format(method))
+
+        return v
+
+    def _crossover(self, v, x, cross, cr):
+        """
+
+        :param v: mutant vector
+        :param x: current vector
+        :param cross: crossover method
+        :param cr: crossover-rate
+        :return:
+        """
+        # crossover
+        r = np.random.choice(range(self._nd))
+        u = np.zeros(self._nd)
+
+        # binary crossover
+        if cross == 'bin':
+            flg = np.equal(r, np.arange(self._nd)) + np.random.rand(self._nd) < cr
+
+        # exponential crossover
+        elif cross == 'exp':
+            flg = np.array([False for _ in range(self._nd)])
+            for l in range(self._nd):
+                flg[r] = True
+                r = (r + 1) % self._nd
+                if np.random.rand() >= cr:
+                    break
+        else:
+            raise ValueError('invalid `cross`: {}'.format(cross))
+
+        # from mutant vector
+        u[flg] = v[flg]
+        # from current vector
+        u[~flg] = x[~flg]
+
+        return u
+
+    def _evaluate_with_check(self, x):
+        if np.any(x < self._low_lim) or np.any(x > self._up_lim):
+            return np.inf
+        else:
+            return self._of(x)
+
+    def _process_1_generation(self, current, gen, method, num, cross, scaling_factor, crossover_rate):
+        # set random seed
+        # seed = now timestamp + current index + current generation
+        seed = int(datetime.datetime.now().timestamp()) + current + gen
+        np.random.seed(seed)
+
+        # mutation
+        v_p = self._get_mutant_vector(current, method, num, scaling_factor)
+
+        # crossover
+        u_p = self._crossover(v_p, self._x_current[current], cross, crossover_rate)
+
+        # evaluation
+        return current, u_p, self._evaluate_with_check(u_p)
+
+    def _exec_1_generation(self, gen, method, num, cross, scaling_factor, crossover_rate, proc):
+        with mp.Pool(proc) as pool:
+            results = pool.map(partial(self._process_1_generation, gen=gen, method=method, num=num, cross=cross,
+                                       scaling_factor=scaling_factor, crossover_rate=crossover_rate), range(self._p))
+
+        u_current = []
+        fu = []
+        for _, u, fp in sorted(results):
+            u_current.append(u)
+            fu.append(fp)
+        return u_current, fu
+
+    def _evaluate(self, current, u):
+        return current, self._evaluate_with_check(u)
+
+    def optimize_mp(self, k_max, method='best', num=1, cross='bin', scaling_factor=0.7, crossover_rate=0.3, proc=None):
+        """
+
+        :param k_max: max-iterations
+        :param method: mutation method ['best', 'rand', 'current-to-best', 'current-to-rand']
+        :param num: number of mutant vectors
+        :param cross: crossover method ['bin', 'exp']
+        :param scaling_factor: scaling-factor F
+        :param crossover_rate: crossover-rate CR
+        :param proc: number of process. if None, then use maximum process
+        :return:
+
+        ex) DE/rand/1/bin --> method='rand', num=1, cross='bin'
+            DE/best/2/exp --> method='best', num=2, cross='exp'
+        """
+        # initialize
+        self.initialization()
+
+        # get fitness of initial x
+        with mp.Pool(proc) as pool:
+            results = pool.starmap(self._evaluate, [(n, u) for n, u in zip(range(self._p), self._x_current)])
+        self._f_current = np.array([r[1] for r in sorted(results)])
+
+        for k in range(k_max):
+
+            u_current, fu = self._exec_1_generation(k, method, num, cross, scaling_factor, crossover_rate, proc)
+
+            # selection
+            self._update(np.stack(u_current, axis=0), np.array(fu))
+
+            best_score = np.amin(self._f_current) if self._is_minimize else np.amax(self._f_current)
+            logger.info('k={} best score = {}'.format(k, best_score))
+
+        # get best point
+        best_idx = np.argmin(self._f_current) if self._is_minimize else np.argmax(self._f_current)
+        x_best = self._x_current[best_idx]
+        logger.info('global best score = {}'.format(self._f_current[best_idx]))
+        logger.info('x_best = {}'.format(x_best))
+        return x_best
+
+    def optimize(self, k_max, method='best', num=1, cross='bin', scaling_factor=0.7, crossover_rate=0.3):
+        """
+
+        :param k_max: max-iterations
+        :param method: mutation method ['best', 'rand', 'current-to-best', 'current-to-rand']
+        :param num: number of mutant vectors
+        :param cross: crossover method ['bin', 'exp']
+        :param scaling_factor: scaling-factor F
+        :param crossover_rate: crossover-rate CR
+        :return:
+
+        ex) DE/rand/1/bin --> method='rand', num=1, cross='bin'
+            DE/best/2/exp --> method='best', num=2, cross='exp'
+        """
+        # initialize
+        self.initialization()
+
+        # get fitness of initial x
+        self._f_current = np.array([self._of(x) for x in self._x_current])
+
+        for k in range(k_max):
+            u_current = []
+            fu = []
+            for p in range(self._p):
+                # mutation
+                v_p = self._get_mutant_vector(p, method=method, num=num, sf=scaling_factor)
+
+                # crossover
+                u_p = self._crossover(v_p, self._x_current[p], cross=cross, cr=crossover_rate)
+
+                # evaluation
+                u_current.append(u_p)
+                fu.append(self._evaluate_with_check(u_p))
+
+            # selection
+            self._update(np.stack(u_current, axis=0), np.array(fu))
+
+            best_score = np.amin(self._f_current) if self._is_minimize else np.amax(self._f_current)
+            logger.info('k={} best score = {}'.format(k, best_score))
+
+        # get best point
+        best_idx = np.argmin(self._f_current) if self._is_minimize else np.argmax(self._f_current)
+        x_best = self._x_current[best_idx]
+        logger.info('global best score = {}'.format(self._f_current[best_idx]))
+        logger.info('x_best = {}'.format(x_best))
+        return x_best
---
+++ tuner.py
@@ -0,0 +1,128 @@
+import numpy as np
+from sklearn.model_selection import KFold
+from tempfile import TemporaryDirectory
+import joblib
+from differential_evolution import DE
+from pathlib import Path
+from logging import getLogger, basicConfig
+
+logger = getLogger('__name__')
+
+
+class HyperTuner(object):
+    def __init__(self, model, space):
+        """
+
+        :param model:
+        :param space:
+
+        space = {
+            'parameter': {'scale': linear', 'range': [0, 1.5]},
+            'parameter': {'scale': 'log', 'range': [-1, 2]},
+            'parameter': {'scale': 'choice', 'range': ['a', 'b', 'c']},
+            'parameter': {'scale': 'integer', 'range': [0, 10]}
+        }
+        """
+        self._model = model
+        assert isinstance(space, dict)
+        self._space = space
+        self._parameters = list(self._space.keys())
+        self._tempdir = TemporaryDirectory()
+        self._tempfile = Path(self._tempdir.name + 'temp_data.gz')
+        self._eval_function = None
+
+    def __del__(self):
+        self._tempdir.cleanup()
+
+    def _get_search_limits(self):
+        lowers = []
+        uppers = []
+        for k in self._parameters:
+            if self._space[k]['scale'] in ['linear', 'log']:
+                lowers.append(self._space[k]['range'][0])
+                uppers.append(self._space[k]['range'][1])
+            elif self._space[k]['scale'] == 'integer':
+                lowers.append(self._space[k]['range'][0])
+                uppers.append(self._space[k]['range'][1] + 1)
+            else:
+                lowers.append(0)
+                uppers.append(len(self._space[k]['range']))
+
+        return np.array(lowers), np.array(uppers)
+
+    def _translate_to_origin(self, x):
+        org_x = {}
+        for n, k in enumerate(self._parameters):
+            if self._space[k]['scale'] == 'log':
+                org_x[k] = np.power(10, x[n])
+            elif self._space[k]['scale'] == 'choice':
+                org_x[k] = self._space[k]['range'][int(x[n])]
+            elif self._space[k]['scale'] == 'integer':
+                org_x[k] = int(x[n])
+            else:
+                org_x[k] = x[n]
+        return org_x
+
+    def _evaluate(self, x):
+        # load data from temporary directory
+        input_data, targets = joblib.load(self._tempfile)
+
+        try:
+            # set model using parameter x
+            param = self._translate_to_origin(x)
+            model = self._model.set_params(**param)
+
+            # train model using CV (K-fold)
+            skf = KFold(n_splits=5, shuffle=True)
+            scores = []
+            for train, test in skf.split(input_data, targets):
+                x_tr, t_tr = input_data[train], targets[train]
+                x_te, t_te = input_data[test], targets[test]
+
+                model.fit(x_tr, t_tr)
+                scores.append(self._eval_function(y_pred=model.predict(x_te), y_true=t_te))
+
+            # average score
+            score = np.average(scores)
+        except Exception as ex:
+            logger.error(ex)
+            # if some errors are occurred, score is +infinity.
+            score = np.inf
+        return score
+
+    def tuning(self, eval_function: callable, x, t, minimize=True):
+        joblib.dump((x, t), self._tempfile)
+
+        # set DE
+        lower_limit, upper_limit = self._get_search_limits()
+
+        # set evaluation function
+        self._eval_function = eval_function
+
+        de = DE(objective_function=self._evaluate, ndim=len(self._parameters), pop=10,
+                lower_limit=lower_limit, upper_limit=upper_limit, minimize=minimize)
+
+        x_best = de.optimize_mp(100, method='best', num=2, cross='bin', scaling_factor=0.7, crossover_rate=0.4)
+
+        return self._translate_to_origin(x_best)
+
+
+if __name__ == '__main__':
+    basicConfig(level='INFO')
+
+    from sklearn.datasets import load_digits
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.metrics import accuracy_score
+
+    search_space = {'n_estimators': {'scale': 'choice', 'range': [10, 50, 100, 200, 250, 300]},
+                    'max_depth': {'scale': 'integer', 'range': [1, 8]},
+                    'min_samples_split': {'scale': 'log', 'range': [-3, 0]},
+                    'min_samples_leaf': {'scale': 'linear', 'range': [0, 0.5]},
+                    'min_weight_fraction_leaf': {'scale': 'linear', 'range': [0, 0.5]},
+                    'max_features': {'scale': 'choice', 'range': ['auto', 'sqrt', 'log2', None]}}
+
+    dataset = load_digits()
+
+    tuner = HyperTuner(model=RandomForestClassifier(), space=search_space)
+    best_param = tuner.tuning(eval_function=accuracy_score, x=dataset.data, t=dataset.target, minimize=False)
+    logger.info('best parameter = {}'.format(best_param))
